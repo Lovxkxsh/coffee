b4b19bd4dc87821dbccf0ca974f704ae99174001
lovxkxsh <lovekeshsharma319@gmail.com>
2025-07-28 15:17:24 +0530

    hello

---
+++ Script/script.js
@@ -0,0 +1,15 @@
+
+const form = document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const status = document.getElementById("status");
+  emailjs.sendForm("service_m6a45t5", "template_nxg6xas", this)
+    .then(function() {
+      status.innerText = ":white_tick: Message sent successfully!";
+      status.style.color = "green";
+    }, function(error) {
+      status.innerText = ":x: Failed to send message. Try again.";
+      status.style.color = "red";
+      console.error("EmailJS Error:", error);
+    });
+  this.reset();
+});
